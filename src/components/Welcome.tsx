565d44493bee72fd0654f1155903ac5de52d5301
Christian Meter <cmeter@googlemail.com>
2022-12-17 14:19:43 +0100

    Add image behind box

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -6,11 +6,13 @@ export default function Example() {
   return (
     <div className="bg-accent px-4 py-5">
       <Container>
-        <div className="relative">
-          <div className="my-7 mx-6 rounded-lg bg-white px-4 py-5 shadow-2xl sm:px-6">
+        <div className="my-12 flex flex-row">
+          <div className="z-10 mx-6 my-7 w-2/3 rounded-lg bg-white px-4 py-5 shadow-2xl sm:px-6">
             <h2 className="pb-3 text-3xl">Willkommen!</h2>
             <p>
               Herzlich willkommen bei der Physiotherapie Gebhard und Schrader!
+            </p>
+            <p>
               Wir sind ein Team von erfahrenen Physiotherapeutinnen, die sich
               auf die Behandlung von Schmerzen und Bewegungsproblemen jeglicher
               Art spezialisiert haben. Unser Ziel ist es, Sie wieder fit und
@@ -19,8 +21,13 @@ export default function Example() {
               kennenzulernen und Ihnen bei Ihren Therapiebedürfnissen zu helfen
             </p>
           </div>
-          <div className="">
-            <Image src={backgroundImage} width={200} height={200} alt="TODO" />
+          <div className="relative -left-24 w-1/3">
+            <Image
+              src={backgroundImage}
+              fill
+              alt="TODO"
+              className="object-contain"
+            />
           </div>
         </div>
       </Container>
