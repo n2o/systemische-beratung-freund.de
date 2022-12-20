a2d7efa2084e0dfb2992da8cee26549004d21f77
Christian Meter <cmeter@googlemail.com>
2022-12-20 16:35:41 +0100

    Add linkable Logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,16 @@
 import { Container } from "@/components/Container"
 import { Logo } from "@/components/Logo"
 import { NavLink } from "@/components/NavLink"
+import Link from "next/link"
 
 export function Footer() {
   return (
     <footer className="bg-slate-50">
       <Container>
         <div className="py-16">
-          <Logo className="mx-auto h-20 w-auto" />
+          <Link href="/">
+            <Logo className="mx-auto h-20 w-auto" />
+          </Link>
           <nav className="mt-10 text-sm" aria-label="quick links">
             <div className="-my-1 flex justify-center gap-x-6">
               <NavLink href="/impressum">Impressum</NavLink>
