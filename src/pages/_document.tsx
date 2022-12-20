0319fc0571a1a0980e6facd0221521d379db1186
Christian Meter <cmeter@googlemail.com>
2022-12-20 12:19:34 +0100

    Add fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,16 +13,6 @@ export default function Document(props) {
           name="description"
           content="Wir sind ein Team von erfahrenen Physiotherapeutinnen, die sich auf die Behandlung von Schmerzen und Bewegungsproblemen jeglicher Art spezialisiert haben."
         />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Lexend:wght@400;500&display=swap"
-        />
       </Head>
       <body className="flex h-full flex-col">
         <Main />
