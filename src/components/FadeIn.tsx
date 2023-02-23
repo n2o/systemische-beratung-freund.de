b81b05f15e1ede6dbcd53adfe7458b115298dc88
Christian Meter <cmeter@googlemail.com>
2023-02-23 09:16:30 +0100

    Add fadeIn to services

--- src/components/FadeIn.tsx
+++ src/components/FadeIn.tsx
@@ -11,8 +11,8 @@ export function FadeIn({ parentRef, children }) {
       enterFrom="opacity-0 scale-50"
       enterTo="opacity-100 scale-100"
       leave="transform duration-200 transition ease-in-out"
-      leaveFrom="opacity-100 scale-100 "
-      leaveTo="opacity-0 scale-95 "
+      leaveFrom="opacity-100 scale-100"
+      leaveTo="opacity-0 scale-95"
     >
       {children}
     </Transition>
--- src/components/Leistungen.tsx
+++ src/components/Leistungen.tsx
@@ -1,3 +1,4 @@
+import { FadeIn } from "@/components/FadeIn"
 import { H2Heading, H2HeadingLeft } from "@/components/Headings"
 import {
   ArrowPathIcon,
@@ -5,8 +6,9 @@ import {
   FingerPrintIcon,
   LockClosedIcon,
 } from "@heroicons/react/24/outline"
+import { useRef } from "react"
 
-const features = [
+const leistungen = [
   {
     name: "Einzelberatung",
     description:
@@ -33,6 +35,30 @@ const features = [
   },
 ]
 
+function Leistung({ leistung }) {
+  const ref = useRef(null)
+  return (
+    <span ref={ref}>
+      <FadeIn parentRef={ref}>
+        <span>
+          <dt className="text-base font-semibold leading-7 text-gray-900">
+            <div className="absolute top-0 left-0 flex h-10 w-10 items-center justify-center rounded-lg bg-primary">
+              <leistung.icon
+                className="h-6 w-6 text-white"
+                aria-hidden="true"
+              />
+            </div>
+            {leistung.name}
+          </dt>
+          <dd className="mt-2 text-base leading-7 text-gray-600">
+            {leistung.description}
+          </dd>
+        </span>
+      </FadeIn>
+    </span>
+  )
+}
+
 export function LeistungsUebersicht() {
   return (
     <div className="bg-white py-8">
@@ -49,20 +75,9 @@ export function LeistungsUebersicht() {
         </H2Heading>
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
           <dl className="grid max-w-xl grid-cols-1 gap-y-10 gap-x-8 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-            {features.map((feature) => (
-              <div key={feature.name} className="relative pl-16">
-                <dt className="text-base font-semibold leading-7 text-gray-900">
-                  <div className="absolute top-0 left-0 flex h-10 w-10 items-center justify-center rounded-lg bg-primary">
-                    <feature.icon
-                      className="h-6 w-6 text-white"
-                      aria-hidden="true"
-                    />
-                  </div>
-                  {feature.name}
-                </dt>
-                <dd className="mt-2 text-base leading-7 text-gray-600">
-                  {feature.description}
-                </dd>
+            {leistungen.map((leistung, leistungIndex) => (
+              <div key={leistungIndex} className="relative pl-16">
+                <Leistung leistung={leistung} />
               </div>
             ))}
           </dl>
