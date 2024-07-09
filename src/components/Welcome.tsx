bc058719f77da36dcea2e09d7375271d02da8d6d
Christian Meter <cmeter@googlemail.com>
2024-07-09 22:29:47 +0200

    Update text

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -6,13 +6,12 @@ export default function Welcome() {
     <Container className="my-12">
       <H1 heading="Herzlich Willkommen" />
       <p>
-        Sehen wir uns nicht alle bei Zeit im Alltag gewissen Herausforderungen
-        ausgesetzt, die in einem Moment kaum der Rede wert sind und an anderer
-        Stelle schier unüberwindbar scheinen? Viel zu oft geraten wir im Leben
-        in Situationen, die wir als Experten in eigener Sache ohne weiteres zu
-        lösen wüssten, wenn da nicht all die äußeren Einflüsse und/oder
-        individuellen Gedankenmodelle wären, die einen konstruktiven,
-        selbstsicheren Blick auf die Dinge erschweren.
+        Sind wir nicht alle im Alltag immer wieder Herausforderungen ausgesetzt,
+        die manchmal unbedeutend erscheinen und an anderer Stelle schier
+        unüberwindbar wirken? Häufig geraten wir in Situationen, die wir als
+        Experten unserer eigenen Angelegenheiten problemlos lösen könnten, wenn
+        nicht all die äußeren Einflüsse und individuellen Denkmuster wären, die
+        unseren konstruktiven und selbstsicheren Blick auf die Dinge erschweren.
       </p>
       <p>
         Trotz der Vielzahl an Angeboten scheint die Hemmschwelle sich dann
