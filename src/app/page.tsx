c2074b24839b2fffa17329b3dccf492ae80a4f74
Christian Meter <cmeter@googlemail.com>
2024-07-12 23:50:12 +0200

    Migrate index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,28 @@
 import { LeistungsUebersicht } from "@/components/Leistungen"
 import { Kurzprofil } from "@/components/Profil"
 import Welcome from "@/components/Welcome"
-import Head from "next/head"
 
-export default function Home() {
+export default function Page() {
   return (
-    <>
-      <Head>
-        <title>Systemische Beratung Freund</title>
-        <meta name="description" content="Systemische Beratung Freund" />
-      </Head>
-
-      <main>
-        <div className="h-96 w-full bg-[url('/img/sandbank.webp')] bg-cover bg-fixed bg-center">
-          <div className="flex h-full w-full items-center justify-center bg-gray-900 bg-opacity-40 py-12">
-            <div className="text-center">
-              <div className="container mx-auto px-4">
-                <div className="mx-auto max-w-4xl text-center">
-                  <span className="text-xl font-semibold text-gray-200 md:text-2xl lg:text-3xl">
-                    &quot;Vielleicht stehen wir uns manchmal selbst im Weg,
-                    damit wir über unsere Bedürfnisse stolpern&quot;
-                  </span>
-                </div>
+    <div>
+      <div className="h-96 w-full bg-[url('/img/sandbank.webp')] bg-cover bg-fixed bg-center">
+        <div className="flex h-full w-full items-center justify-center bg-gray-900 bg-opacity-40 py-12">
+          <div className="text-center">
+            <div className="container mx-auto px-4">
+              <div className="mx-auto max-w-4xl text-center">
+                <span className="text-xl font-semibold text-gray-200 md:text-2xl lg:text-3xl">
+                  &quot;Vielleicht stehen wir uns manchmal selbst im Weg, damit
+                  wir über unsere Bedürfnisse stolpern&quot;
+                </span>
               </div>
             </div>
           </div>
         </div>
+      </div>
 
-        <Welcome />
-        <Kurzprofil />
-        <LeistungsUebersicht />
-      </main>
-    </>
+      <Welcome />
+      <Kurzprofil />
+      <LeistungsUebersicht />
+    </div>
   )
 }
