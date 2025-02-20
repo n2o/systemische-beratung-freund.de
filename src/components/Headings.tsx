262e000772d5891ec8757c078efcd4b1647dfb13
Christian Meter <cmeter@googlemail.com>
2025-02-21 00:03:18 +0100

    Refactor H2 component: update text styling for improved readability and visual hierarchy

--- src/components/Headings.tsx
+++ src/components/Headings.tsx
@@ -3,10 +3,10 @@ import { Container } from "@/components/Container"
 export function H2({ heading = "", leadHeading = "", children }) {
   return (
     <div className="mx-auto max-w-4xl lg:text-center">
-      <h2 className="text-lg font-semibold leading-8 tracking-tight">
+      <h2 className="text-lg font-medium leading-8 tracking-tight">
         {leadHeading}
       </h2>
-      <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+      <p className="mt-2 text-3xl font-light tracking-tight text-gray-900 sm:text-4xl">
         {heading}
       </p>
       <div className="mt-6 text-lg leading-8 text-gray-600">{children}</div>
